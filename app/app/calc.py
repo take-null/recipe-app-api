16cd52358c8af5cd7e2812b3876d0ea3c759266f
毅彦 温 <Takehiko.Nurui@soliton.co.jp>
2022-12-24 03:57:21 +0900

    fix

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,11 +1,11 @@
 """
-Calculater functions
+Calculator functions
 """
-
+ 
 def add(x, y):
-    """add x and y and return result."""
+    """Add x and y and return result."""
     return x + y
-
+ 
 def subtract(x, y):
     """Subtract x from y and return result."""
     return y - x
--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -4,7 +4,7 @@ Test custom Django management commands.
 
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
+from psycopg2 import OperationalError as Psycopg2OpError
 
 from django.core.management import call_command
 from django.db.utils import OperationalError
@@ -20,4 +20,4 @@ class CommandTests(SimpleTestCase):
 
         call_command('wait_for_db')
 
-        patched_check.assert_called_once_with(database=['default'])
+        patched_check.assert_called_once_with(databases=['default'])
