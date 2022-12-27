79bea8a83ef41682ceea0912e91e616027b1a0e9
毅彦 温 <Takehiko.Nurui@soliton.co.jp>
2022-12-28 02:32:18 +0900

    create recipe app

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -103,7 +103,7 @@ class PublicUserApiTests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
 
     def test_create_token_blank_password(self):
-        """Test posting a blank password return an error."""
+        """Test posting a blank password returns an error."""
         payload = {'email': 'test@example.com', 'password': ''}
         res = self.client.post(TOKEN_URL, payload)
 
