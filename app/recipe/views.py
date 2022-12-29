739a78aa6a484dafab78da5fcdde65e89b566c08
毅彦 温 <Takehiko.Nurui@soliton.co.jp>
2022-12-29 20:10:49 +0900

    t

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -13,11 +13,9 @@ class RecipeViewSet(viewsets.ModelViewSet):
     """View for manage recipe APIs."""
     serializer_class = serializers.RecipeSerializer
     queryset = Recipe.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
         """Retrieve recipes for authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-id')
-
-
