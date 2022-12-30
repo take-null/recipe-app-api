2a30f138143203a039335a705a930e9f797f8eab
毅彦 温 <Takehiko.Nurui@soliton.co.jp>
2022-12-30 15:26:25 +0900

    fix

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -38,7 +38,10 @@ class RecipeViewSet(viewsets.ModelViewSet):
         serializer.save(user=self.request.user)
 
 
-class TagViewSet(mixins.ListModelMixin, viewsets.GenericViweSet):
+class TagViewSet(mixins.DestroyModelMixin,
+                 mixins.UpdateModelMixin,
+                 mixins.ListModelMixin,
+                 viewsets.GenericViewSet):
     """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
@@ -48,4 +51,3 @@ class TagViewSet(mixins.ListModelMixin, viewsets.GenericViweSet):
     def get_queryset(self):
         """Filter queryset to authenticated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
-
